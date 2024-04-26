6961fa471f60018631fe34699e91bb1ce71a4a0e
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2024-04-25 17:20:04 -0700

    initial commit

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Bookmark = () => {
+	return (
+		<SafeAreaView>
+			<Text>Bookmark</Text>
+			{/* todo: finish this page */}
+			<Text>Under Construction</Text>
+		</SafeAreaView>
+	);
+};
+
+export default Bookmark;
